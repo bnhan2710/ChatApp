e82ee9900b03ac6796049b9fac7ca9c12ecfba31
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-08-08 16:03:49 +0700

    base ui

--- public/js/client.js
+++ public/js/client.js
@@ -4,8 +4,10 @@ const btn_join = document.getElementById('btn_join');
 
 const ip_message = document.getElementById('ip_message');
 const btn_send = document.getElementById('btn_send');
-const ul_message = document.getElementById('ul_message')
+const ul_message = document.getElementById('ul_message');
+
 let socket = io.connect();
+let currentUserName = ''; 
 
 socket.on("connect", () => {
     console.log('Connected to server');
@@ -15,25 +17,49 @@ btn_join.addEventListener('click', () => {
     const name = ip_name.value;
     const room = ip_room.value;
     if (name && room) {
+        currentUserName = name; 
         socket.emit("join", { name, room });
+        alert(`Join room ${room} successfully !!`)
     } else {
         alert('Please enter both name and room');
     }
 });
 
-btn_send.addEventListener('click', () => {
+const sendmsg = () => {
+    console.log('send message');
     const message = ip_message.value;
-    const name = ip_name.value;
-    if (message && room) {
-        socket.emit("message", { name, message });
+    const obj ={ 
+        name:currentUserName,
+         message 
+        }
+    if (message) {
+        socket.emit("message",JSON.stringify(obj));
         ip_message.value = '';
     } else {
         alert('Please enter a message');
     }
-});
+}
+
+btn_send.addEventListener('click', sendmsg);
+
 
 socket.on("thread", (data) => {
-    const li = document.createElement("li")
-    li.innerText = ` - ${data.name}: ${data.message}`
-    ul_message.appendChild(li)
+    const obj = JSON.parse(data)
+    const li = document.createElement("li");
+    li.innerText = `${obj.message}`;
+    
+    if (obj.name === currentUserName) {
+        li.classList.add('message-self');
+    } else {
+        li.classList.add('message-other');
+    }
+    
+    ul_message.appendChild(li);
+    ul_message.scrollTop = ul_message.scrollHeight; 
 });
+
+ip_message.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter') {
+        sendmsg();
+    }
+})
